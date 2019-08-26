20ced9eec7188d4f5a5df574bbb4817a3fb711f4
the-phoenix <9201924+the-phoenix@users.noreply.github.com>
2019-08-27 04:52:44 +0800

    update web db settings to match with docker

--- TinPennyBackend/settings.py
+++ TinPennyBackend/settings.py
@@ -78,11 +78,12 @@ WSGI_APPLICATION = 'TinPennyBackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': os.environ.get('SQL_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get('SQL_DATABASE', os.path.join(BASE_DIR, 'db.sqlite3')),
+        'USER': os.environ.get('SQL_USER', 'user'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD', 'password'),
+        'HOST': os.environ.get('SQL_HOST', 'localhost'),
+        'PORT': os.environ.get('SQL_PORT', '5432'),
     }
 }
 

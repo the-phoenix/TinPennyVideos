88403d2c4c64707f7249506569a9499a645d8a61
the-phoenix <9201924+the-phoenix@users.noreply.github.com>
2019-08-27 04:41:09 +0800

    read django settings from env var

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,17 @@
+# pull official base image
 FROM python:3
+
+# set environment variables
 ENV PYTHONUNBUFFERED 1
+
+# set work directory
 RUN mkdir /code
 WORKDIR /code
+
+# install dependencies
 COPY requirements.txt /code/
+RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
+
+# copy project
 COPY . /code/
--- TinPennyBackend/settings.py
+++ TinPennyBackend/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wof=$gx65c8a_arpu7o!-kkqj9o&h$!e#+kw)rhh-^b59-x&11'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition

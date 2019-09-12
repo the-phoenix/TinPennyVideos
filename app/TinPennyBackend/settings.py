0651075d87dfa1f6599db6be6aed26fc9b075bf9
the-phoenix <9201924+the-phoenix@users.noreply.github.com>
2019-09-13 00:25:00 +0800

    fix some discrepancy

--- app/TinPennyBackend/settings.py
+++ app/TinPennyBackend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))

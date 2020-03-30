4a0f6f6658351921abdb5033c375cd2d69ee4c85
the-phoenix <9201924+the-phoenix@users.noreply.github.com>
2020-03-29 21:38:32 -0400

    update frontend

--- frontend/src/components/LayoutComponents/Footer/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Footer = () => (
-  <div>
-    This is Footer!
-  </div>
-)
-
-export default Footer
--- frontend/src/redux/category/actions.js
+++
@@ -1,9 +0,0 @@
-import { createActions } from "reduxsauce";
-
-const { Types, Creators } = createActions({
-  setState: ["payload"],
-  loadCategories: null,
-}, {prefix: 'C_'});
-
-export const types = Types;
-export default Creators;
